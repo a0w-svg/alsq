0a25b93bbe11f330a75a4305bc1af1447362c80e
a0w-svg <dawidkochanski86@gmail.com>
2020-06-23 10:15:06 +0200

    add python discord bot

--- ALSQ@/commands/role.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'role',
-    description: 'add role to user',
-    args: true,
-    usage: '<user> <role>',
-    execute(message, args) {
-
-    }
-};
--- ALSQ@/commands/user-info.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'user-info',
-    description: 'Display info about yourself',
-    execute(message) {
-
-        message.channel.send(`Your Name: ${message.author.username}\nYour ID: ${message.author.id}`);
-    },
-};
