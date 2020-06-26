e0d24f0079dfd100822e7b7675ff748f330057d0
a0aRd <dawidkochanski86@gmail.com>
2020-06-26 21:30:19 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,13 @@ import youtube_dl
 import random
 
 description = '''ALSQ bot sample '''
-
+if not discord.opus.is_loaded():
+    # the 'opus' library here is opus.dll on windows
+    # or libopus.so on linux in the current directory
+    # you should replace this with the location the
+    # opus library is located in and with the proper filename.
+    # note that on windows this DLL is automatically provided for you
+    discord.opus.load_opus('opus')
 bot = commands.Bot(command_prefix='$', description=description)
 
 @bot.event
